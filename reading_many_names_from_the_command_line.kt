9f76f38c5d43a4b9434d2884004eb1c07ea7fd4d
Sale <sale@debiantest1.test.dev>
2018-03-08 16:18:52 +0100

    Reading many names from the command line

---
+++ reading_many_names_from_the_command_line.kt
@@ -0,0 +1,10 @@
+/**
+ * Line 2 demonstrates the for-loop, that would have beed called "enhanced"
+ * if there were any other for-loop in Kotlin.
+ * See http://kotlinlang.org/docs/reference/basic-syntax.html#using-a-for-loop
+ */
+
+fun main(args: Array<String>) {
+    for (name in args)
+	println("Helllo, $name!")
+}
